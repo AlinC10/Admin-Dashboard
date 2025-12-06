455f9a80eb3b7487f1d42437be5ef03995cb02de
AlinC10 <alincalcan@gmail.com>
2025-12-06 15:44:10 +0200

    Add logic for responsivness
    - add javascript to cut text if it overflows
    - fix one icon
    - add photo to "The Royal Hunt"

---
+++ script.js
@@ -0,0 +1,27 @@
+let para = document.querySelectorAll(".left .box p");
+const suffixText = " ...(More on GitHub)";
+const maxHeight = 85;
+
+para.forEach((p) => {
+    p.setAttribute("original-text", p.textContent.trim());
+});
+
+function handleResize() {
+    para.forEach((p) => {
+        let originalText = p.getAttribute("original-text");
+        p.textContent = originalText;
+        let change = false;
+        while (p.scrollHeight > p.clientHeight) {
+            originalText = originalText.slice(0, -5);
+            p.textContent = originalText + suffixText;
+            change = true;
+        }
+        if (change) {
+            p.innerHTML = `${originalText}<span>${suffixText}</span>`;
+        }
+    });
+}
+
+window.addEventListener("resize", handleResize);
+
+handleResize(); // if user enters from mobile
